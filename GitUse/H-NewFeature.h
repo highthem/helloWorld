bfc7f8bee78e0461413f5f1b0f590df076d2b831
Haithem BH <highthem@highthem.local>
2014-04-01 12:05:38 +0200

    + add new feature

---
+++ GitUse/H-NewFeature.h
@@ -0,0 +1,13 @@
+//
+//  H-NewFeature.h
+//  GitUse
+//
+//  Created by Haithem BH on 01/04/2014.
+//  Copyright (c) 2014 highthem®. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface H_NewFeature : NSObject
+
+@end
---
+++ GitUse/H-NewFeature.m
@@ -0,0 +1,13 @@
+//
+//  H-NewFeature.m
+//  GitUse
+//
+//  Created by Haithem BH on 01/04/2014.
+//  Copyright (c) 2014 highthem®. All rights reserved.
+//
+
+#import "H-NewFeature.h"
+
+@implementation H_NewFeature
+
+@end
